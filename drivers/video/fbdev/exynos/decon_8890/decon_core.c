b1d3c0d1a77e62b7dc712275d3cfdad3483e9eaa
lyapota <lyapic@ukr.net>
2016-07-17 10:57:42 +0300

    Powersuspend v1.7 [Fau123, Yank555.lu]

--- drivers/video/fbdev/exynos/decon_8890/decon_core.c
+++ drivers/video/fbdev/exynos/decon_8890/decon_core.c
@@ -33,6 +33,10 @@
 #include <linux/debugfs.h>
 #include <linux/of_gpio.h>
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #include <media/exynos_mc.h>
 #include <video/mipi_display.h>
 #include <media/v4l2-subdev.h>
@@ -1520,6 +1524,9 @@ static int decon_blank(int blank_mode, struct fb_info *info)
 	case FB_BLANK_NORMAL:
 		DISP_SS_EVENT_LOG(DISP_EVT_BLANK, &decon->sd, ktime_set(0, 0));
 		ret = decon_disable(decon);
+#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
+#endif
 		if (ret) {
 			decon_err("failed to disable decon\n");
 			goto blank_exit;
@@ -1528,6 +1535,9 @@ static int decon_blank(int blank_mode, struct fb_info *info)
 	case FB_BLANK_UNBLANK:
 		DISP_SS_EVENT_LOG(DISP_EVT_UNBLANK, &decon->sd, ktime_set(0, 0));
 		ret = decon_enable(decon);
+#ifdef CONFIG_POWERSUSPEND
+		set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
+#endif
 		if (ret) {
 			decon_err("failed to enable decon\n");
 			goto blank_exit;
