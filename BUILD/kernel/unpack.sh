7937242c4eb33a04a5140a51ef7a97c77c2449cb
lyapota <lyapic@ukr.net>
2016-07-18 23:18:42 +0300

    universal aroma installer for G930F/G935F

--- BUILD/kernel/unpack.sh
+++ BUILD/kernel/unpack.sh
@@ -4,8 +4,29 @@
 # ========================================
 # Created by lyapota
 
-echo "unpack boot.img"
-mv -f /tmp/boot.img /tmp/AIK
+get_sel()
+{
+  sel_file=$DIR_SEL/$1
+  sel_value=`cat $sel_file | cut -d '=' -f2`
+  echo $sel_value
+}
+
+if [ ! -e $DIR_SEL/pr_device.prop ]; then
+	BOOT="boot.img"
+else
+	val1=`get_sel pr_device.prop`
+        case $val1 in
+        	1)
+		  BOOT="boot-hero2lte.img"
+        	  ;;
+        	2)
+		  BOOT="boot-herolte.img"
+        	  ;;
+        esac
+fi
+
+echo "unpack $BOOT"
+mv -f /tmp/"$BOOT" /tmp/AIK/boot.img
 
 cd /tmp/AIK
 chmod 755 unpackimg.sh;
