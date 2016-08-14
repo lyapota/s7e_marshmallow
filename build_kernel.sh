d5be97693755ff309cb5be9400326b88c59361f6
lyapota <lyapic@ukr.net>
2016-08-15 01:34:19 +0300

    set version 1.4.0

--- build_kernel.sh
+++ build_kernel.sh
@@ -32,7 +32,7 @@ BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
 export PATH=$(pwd)/bin:$PATH
 
-KERNEL_VERSION="1.3.2"
+KERNEL_VERSION="1.4.0"
 KERNEL_NAME="-prometheus"
 export LOCALVERSION=${KERNEL_NAME}-v${KERNEL_VERSION}
 
