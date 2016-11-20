435d9b847e80fe30eeadc2a97c0af49f925e2bbe
lyapota <lyapic@ukr.net>
2016-11-20 11:25:28 +0200

    set versin 2.0.0

--- build_kernel.sh
+++ build_kernel.sh
@@ -32,7 +32,7 @@ BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
 export PATH=$(pwd)/bin:$PATH
 
-KERNEL_VERSION="1.9.0"
+KERNEL_VERSION="2.0.0"
 KERNEL_NAME="-prometheus"
 export LOCALVERSION=${KERNEL_NAME}-v${KERNEL_VERSION}
 
